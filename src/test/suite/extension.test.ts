8d209b73ab1bc118e83bbde569dccfd6d756603a
Amirmasoud <amirmasood33@gmail.com>
2020-06-25 00:01:14 +0430

    WIP

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -7,13 +7,6 @@ import * as Oops from '../../extension';
 const testFolderLocation = '/../../../src/test/example/';
 
 suite('Extension Test Suite', () => {
-	vscode.window.showInformationMessage('Start all tests.');
-
-	test('Sample test', () => {
-		assert.equal(-1, [1, 2, 3].indexOf(5));
-		assert.equal(-1, [1, 2, 3].indexOf(0));
-	});
-
 	test('Should change file to folder', async () => {
 		const uri = vscode.Uri.file(
 			path.join(__dirname + testFolderLocation + 'oops_file.js')
