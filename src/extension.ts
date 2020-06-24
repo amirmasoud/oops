43db28bc26d8fa1c24f002ddc1365f6918098fb9
Amirmasoud <amirmasood33@gmail.com>
2020-06-24 23:16:27 +0430

    Tests

--- src/extension.ts
+++ src/extension.ts
@@ -1,40 +1,47 @@
 import * as vscode from 'vscode';
 import * as fs from 'fs';
-import * as mkdirp from 'mkdirp';
-import * as rimraf from 'rimraf';
 import { FileSystemProvider, FileStat } from './fileExplorer';
 
 export function activate(context: vscode.ExtensionContext) {
     const dispose = vscode.commands.registerCommand('oops.swap', async (uri: vscode.Uri) => {
-        const fileState = new FileStat(fs.lstatSync(uri.fsPath));
-        const provider = new FileSystemProvider();
-        if (fileState.isFile) {
-            const content = await provider.readFile(uri);
-            if (content.length) {
-                vscode.window.showErrorMessage('File is not empty.');
-            } else {
-                await provider.delete(uri, { recursive: false });
-                vscode.commands.executeCommand('workbench.files.action.refreshFilesExplorer');
-                await provider.createDirectory(uri);
-                vscode.commands.executeCommand('workbench.files.action.refreshFilesExplorer');
-            }
-        }
-
-        if (fileState.isDirectory) {
-            const children = await provider.readDirectory(uri);
-            if (children.length) {
-                vscode.window.showErrorMessage('Directory is not empty.');
-            } else {
-                const newUri = vscode.Uri.file(uri.fsPath);
-                await provider.delete(uri, { recursive: true });
-                vscode.commands.executeCommand('workbench.files.action.refreshFilesExplorer');
-                const content = new Uint8Array(0);
-                await provider.writeFile(newUri, content, { create: true, overwrite: true });
-                vscode.commands.executeCommand('workbench.files.action.refreshFilesExplorer');
-            }
-        }
+        fileToFolder(uri);
+        folderToFile(uri);
     });
     context.subscriptions.push(dispose);
+}
+
+export async function fileToFolder(uri: vscode.Uri) {
+    const fileState = new FileStat(fs.lstatSync(uri.fsPath));
+    const provider = new FileSystemProvider();
+    if (fileState.isFile) {
+        const content = await provider.readFile(uri);
+        if (content.length) {
+            vscode.window.showErrorMessage('File is not empty.');
+        } else {
+            await provider.delete(uri, { recursive: false });
+            vscode.commands.executeCommand('workbench.files.action.refreshFilesExplorer');
+            await provider.createDirectory(uri);
+            vscode.commands.executeCommand('workbench.files.action.refreshFilesExplorer');
+        }
+    }
+}
+
+export async function folderToFile(uri: vscode.Uri) {
+    const fileState = new FileStat(fs.lstatSync(uri.fsPath));
+    const provider = new FileSystemProvider();
+    if (fileState.isDirectory) {
+        const children = await provider.readDirectory(uri);
+        if (children.length) {
+            vscode.window.showErrorMessage('Directory is not empty.');
+        } else {
+            const newUri = vscode.Uri.file(uri.fsPath);
+            await provider.delete(uri, { recursive: true });
+            vscode.commands.executeCommand('workbench.files.action.refreshFilesExplorer');
+            const content = new Uint8Array(0);
+            await provider.writeFile(newUri, content, { create: true, overwrite: true });
+            vscode.commands.executeCommand('workbench.files.action.refreshFilesExplorer');
+        }
+    }
 }
 
 export function deactivate() { }
--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -1,9 +1,10 @@
+import * as fs from 'fs';
+import * as path from 'path';
 import * as assert from 'assert';
-
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
 import * as vscode from 'vscode';
-// import * as myExtension from '../../extension';
+import * as Oops from '../../extension';
+
+const testFolderLocation = '/../../../src/test/example/';
 
 suite('Extension Test Suite', () => {
 	vscode.window.showInformationMessage('Start all tests.');
@@ -11,5 +12,21 @@ suite('Extension Test Suite', () => {
 	test('Sample test', () => {
 		assert.equal(-1, [1, 2, 3].indexOf(5));
 		assert.equal(-1, [1, 2, 3].indexOf(0));
+	});
+
+	test('Should change file to folder', async () => {
+		const uri = vscode.Uri.file(
+			path.join(__dirname + testFolderLocation + 'oops_file.js')
+		);
+		await Oops.fileToFolder(uri);
+		assert.ok(fs.lstatSync(uri.fsPath).isDirectory());
+	});
+
+	test('Should change folder to file', async () => {
+		const uri = vscode.Uri.file(
+			path.join(__dirname + testFolderLocation + 'oops_folder')
+		);
+		await Oops.folderToFile(uri);
+		assert.ok(fs.lstatSync(uri.fsPath).isFile());
 	});
 });
