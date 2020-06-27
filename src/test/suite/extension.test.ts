078739691ea4d9b9c4cb0f9584ddf3a665c780bb
Amirmasoud <amirmasood33@gmail.com>
2020-06-27 23:10:35 +0430

    Increase timeout for Mocha instance

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -7,20 +7,20 @@ import * as Oops from '../../extension';
 const testFolderLocation = '/../../../src/test/example/';
 
 suite('Extension Test Suite', () => {
-	test('Should change file to folder', async () => {
+	test('Should change folder to file', async () => {
 		const uri = vscode.Uri.file(
 			path.join(__dirname + testFolderLocation + 'oops_file.js')
 		);
-		await Oops.fileToFolder(uri);
-		assert.ok(fs.lstatSync(uri.fsPath).isDirectory());
+		await Oops.folderToFile(uri);
+		assert.ok(fs.lstatSync(uri.fsPath).isFile());
 	});
 
-	test('Should change folder to file', async () => {
+	test('Should change file to folder', async () => {
 		const uri = vscode.Uri.file(
 			path.join(__dirname + testFolderLocation + 'oops_folder')
 		);
-		await Oops.folderToFile(uri);
-		assert.ok(fs.lstatSync(uri.fsPath).isFile());
+		await Oops.fileToFolder(uri);
+		assert.ok(fs.lstatSync(uri.fsPath).isDirectory());
 	});
 
 	test('Should not be able to delete not empty directory', async () => {
