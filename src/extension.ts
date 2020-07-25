2c6c1f9310a9aa73b9e8d588cfe621b2602487ca
AmirMasoud <amirmasood33@gmail.com>
2020-07-25 09:30:29 +0430

    Fix default document formatter

--- src/extension.ts
+++ src/extension.ts
@@ -3,27 +3,27 @@ import * as vscode from "vscode";
 import { FileSystemProvider, FileStat } from "./fileExplorer";
 
 export function activate(context: vscode.ExtensionContext) {
-  // Activate extension command `oops.swap`
-  const dispose = vscode.commands.registerCommand(
-    "oops.swap",
-    async (uri: vscode.Uri) => {
-      // Get information about selected symbolic link
-      const fileState = new FileStat(fs.lstatSync(uri.fsPath));
+	// Activate extension command `oops.swap`
+	const dispose = vscode.commands.registerCommand(
+		"oops.swap",
+		async (uri: vscode.Uri) => {
+			// Get information about selected symbolic link
+			const fileState = new FileStat(fs.lstatSync(uri.fsPath));
 
-      // We have only 2 methods in this extension, and we need to invoke as follow:
-      // 1. if it's a file:   invoke function to convert file to folder
-      // 2. if it's a folder: invoke function to convert folder to file
-      if (fileState.isFile) {
-        fileToFolder(uri);
-      }
+			// We have only 2 methods in this extension, and we need to invoke as follow:
+			// 1. if it's a file:   invoke function to convert file to folder
+			// 2. if it's a folder: invoke function to convert folder to file
+			if (fileState.isFile) {
+				fileToFolder(uri);
+			}
 
-      if (fileState.isDirectory) {
-        folderToFile(uri);
-      }
-    }
-  );
+			if (fileState.isDirectory) {
+				folderToFile(uri);
+			}
+		}
+	);
 
-  context.subscriptions.push(dispose);
+	context.subscriptions.push(dispose);
 }
 
 /**
@@ -31,75 +31,75 @@ export function activate(context: vscode.ExtensionContext) {
  *
  * @param uri file URI
  */
-export async function fileToFolder(uri: vscode.Uri) {
-  const provider = new FileSystemProvider();
+export async function fileToFolder(uri: vscode.Uri): Promise<void> {
+	const provider = new FileSystemProvider();
 
-  // Read file content and check its content to see if it's empty.
-  const content = await provider.readFile(uri);
-  const isEmpty = content.every((el, inx, arr) => {
-    const emptyChars = [
-      // Tab
-      9,
-      // Line Feed
-      10,
-      // Carriage Return
-      13, 
-      // Space
-      32
-    ];
-    return emptyChars.includes(el);
-  });
+	// Read file content and check its content to see if it's empty.
+	const content = await provider.readFile(uri);
+	const isEmpty = content.every((el, inx, arr) => {
+		const emptyChars = [
+			// Tab
+			9,
+			// Line Feed
+			10,
+			// Carriage Return
+			13,
+			// Space
+			32,
+		];
+		return emptyChars.includes(el);
+	});
 
-  // Basic check: `content.length`
-  if (isEmpty) {
-    vscode.window.showErrorMessage("File is not empty.");
-  } else {
-    // Remove given file URI and refresh files explorer
-    await provider.delete(uri, { recursive: false });
-    vscode.commands.executeCommand(
-      "workbench.files.action.refreshFilesExplorer"
-    );
+	// Basic check: `content.length`
+	if (isEmpty) {
+		vscode.window.showErrorMessage("File is not empty.");
+	} else {
+		// Remove given file URI and refresh files explorer
+		await provider.delete(uri, { recursive: false });
+		vscode.commands.executeCommand(
+			"workbench.files.action.refreshFilesExplorer"
+		);
 
-    // Create new directory with same file URI and refresh files explorer
-    await provider.createDirectory(uri);
-    vscode.commands.executeCommand(
-      "workbench.files.action.refreshFilesExplorer"
-    );
-  }
+		// Create new directory with same file URI and refresh files explorer
+		await provider.createDirectory(uri);
+		vscode.commands.executeCommand(
+			"workbench.files.action.refreshFilesExplorer"
+		);
+	}
 }
 
 /**
  * convert given folder URI to file.
- * 
+ *
  * @param uri folder URI
  */
-export async function folderToFile(uri: vscode.Uri) {
-  const provider = new FileSystemProvider();
+export async function folderToFile(uri: vscode.Uri): Promise<void> {
+	const provider = new FileSystemProvider();
 
-  // Read directory children to check we won't mess users' projects
-  const children = await provider.readDirectory(uri);
-  if (children.length) {
-    vscode.window.showErrorMessage("Directory is not empty.");
-  } else {
-    // Init new file with same URI as folder URI
-    const newUri = vscode.Uri.file(uri.fsPath);
+	// Read directory children to check we won't mess users' projects
+	const children = await provider.readDirectory(uri);
+	if (children.length) {
+		vscode.window.showErrorMessage("Directory is not empty.");
+	} else {
+		// Init new file with same URI as folder URI
+		const newUri = vscode.Uri.file(uri.fsPath);
 
-    // Remove folder and refresh files explorer
-    await provider.delete(uri, { recursive: true });
-    vscode.commands.executeCommand(
-      "workbench.files.action.refreshFilesExplorer"
-    );
+		// Remove folder and refresh files explorer
+		await provider.delete(uri, { recursive: true });
+		vscode.commands.executeCommand(
+			"workbench.files.action.refreshFilesExplorer"
+		);
 
-    // Empty content for new file
-    const content = new Uint8Array(0);
+		// Empty content for new file
+		const content = new Uint8Array(0);
 
-    // Write file we created earlier and write empty content into it and refresh files explorer
-    await provider.writeFile(newUri, content, {
-      create: true,
-      overwrite: true,
-    });
-    vscode.commands.executeCommand(
-      "workbench.files.action.refreshFilesExplorer"
-    );
-  }
+		// Write file we created earlier and write empty content into it and refresh files explorer
+		await provider.writeFile(newUri, content, {
+			create: true,
+			overwrite: true,
+		});
+		vscode.commands.executeCommand(
+			"workbench.files.action.refreshFilesExplorer"
+		);
+	}
 }
--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -7,61 +7,61 @@ import * as Oops from "../../extension";
 const testFolderLocation = "/../../../src/test/example/";
 
 suite("Extension Test Suite", () => {
-  test("Should change folder to file", () => {
-    const uri = vscode.Uri.file(
-      path.join(__dirname + testFolderLocation + "oops_file.js")
-    );
+	test("Should change folder to file", () => {
+		const uri = vscode.Uri.file(
+			path.join(__dirname + testFolderLocation + "oops_file.js")
+		);
 
-    Oops.folderToFile(uri).then(() => {
-      assert.ok(fs.lstatSync(uri.fsPath).isFile());
-    });
+		Oops.folderToFile(uri).then(() => {
+			assert.ok(fs.lstatSync(uri.fsPath).isFile());
+		});
 
-    Oops.fileToFolder(uri).then(() => {
-      assert.ok(fs.lstatSync(uri.fsPath).isDirectory());
-    });
-  });
+		Oops.fileToFolder(uri).then(() => {
+			assert.ok(fs.lstatSync(uri.fsPath).isDirectory());
+		});
+	});
 
-  test("Should change file to folder", () => {
-    const uri = vscode.Uri.file(
-      path.join(__dirname + testFolderLocation + "oops_folder")
-    );
+	test("Should change file to folder", () => {
+		const uri = vscode.Uri.file(
+			path.join(__dirname + testFolderLocation + "oops_folder")
+		);
 
-    fs.rename(
-      testFolderLocation + "oops_folder/.gitkeep",
-      testFolderLocation + ".gitkeep",
-      () => {
-        Oops.fileToFolder(uri).then(() => {
-          assert.ok(fs.lstatSync(uri.fsPath).isDirectory());
-        });
+		fs.rename(
+			testFolderLocation + "oops_folder/.gitkeep",
+			testFolderLocation + ".gitkeep",
+			() => {
+				Oops.fileToFolder(uri).then(() => {
+					assert.ok(fs.lstatSync(uri.fsPath).isDirectory());
+				});
 
-        Oops.folderToFile(uri).then(() => {
-          assert.ok(fs.lstatSync(uri.fsPath).isFile());
-        });
+				Oops.folderToFile(uri).then(() => {
+					assert.ok(fs.lstatSync(uri.fsPath).isFile());
+				});
 
-        fs.rename(
-          testFolderLocation + ".gitkeep",
-          testFolderLocation + "oops_folder/.gitkeep",
-          () => {}
-        );
-      }
-    );
-  });
+				fs.rename(
+					testFolderLocation + ".gitkeep",
+					testFolderLocation + "oops_folder/.gitkeep",
+					() => { }
+				);
+			}
+		);
+	});
 
-  test("Should not be able to delete not empty directory", async () => {
-    const uri = vscode.Uri.file(
-      path.join(__dirname + testFolderLocation + "not_empty_directory")
-    );
-    await Oops.folderToFile(uri);
-    assert.ok(fs.lstatSync(uri.fsPath).isDirectory());
-    assert.ok(fs.lstatSync(uri.fsPath + "/directory_with_children").isFile());
-  });
+	test("Should not be able to delete not empty directory", async () => {
+		const uri = vscode.Uri.file(
+			path.join(__dirname + testFolderLocation + "not_empty_directory")
+		);
+		await Oops.folderToFile(uri);
+		assert.ok(fs.lstatSync(uri.fsPath).isDirectory());
+		assert.ok(fs.lstatSync(uri.fsPath + "/directory_with_children").isFile());
+	});
 
-  test("Should not be able to delete not empty file", async () => {
-    const uri = vscode.Uri.file(
-      path.join(__dirname + testFolderLocation + "not_empty_file")
-    );
+	test("Should not be able to delete not empty file", async () => {
+		const uri = vscode.Uri.file(
+			path.join(__dirname + testFolderLocation + "not_empty_file")
+		);
 
-    await Oops.fileToFolder(uri);
-    assert.ok(fs.lstatSync(uri.fsPath).isFile());
-  });
+		await Oops.fileToFolder(uri);
+		assert.ok(fs.lstatSync(uri.fsPath).isFile());
+	});
 });
