5dec71cb6b013eb0e63e2ce4fe164ef8167e80f5
AmirMasoud <amirmasood33@gmail.com>
2020-09-03 09:03:43 +0430

    Fix test suites and add .gitatributes

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,36 +5,58 @@ All notable changes to the "oops" extension will be documented in this file.
 Check [Keep a Changelog](http://keepachangelog.com/) for recommendations on how to structure this file.
 
 ## [Unreleased]
+
 ---
 
-## [1.1.4] - 2020-07-01
+## [1.1.5] - 2020-XX-XX
+
 ### Changed
+
+- Fix tests
+- Add .gitattributes
+
+## [1.1.4] - 2020-07-01
+
+### Changed
+
 - changed galleryBanner color to #FFF66E
 
 ## [1.1.3] - 2020-07-01
+
 ### Changed
+
 - changed release note to changelog in readme
 
 ## [1.1.2] - 2020-07-01
+
 ### Changed
+
 - Change galleryBanner theme to light
 
 ## [1.1.1] - 2020-07-01
+
 ### Fixed
+
 - Fix icon path
 
 ### Removed
+
 - Removed quick start file
 
 ### Added
+
 - galleryBanner added to package.json
 
 ### Changed
+
 - Change src/images/icon.png to images/icon.png
 
 ## [1.1.0] - 2020-07-01
+
 ### Added
+
 - Add icon, categories, keywords and, badges to package.json
 
 ## [1.0.0] - 2020-06-27
+
 - Initial release
--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -12,35 +12,21 @@ suite("Extension Test Suite", () => {
 			path.join(__dirname + testFolderLocation + "oops_file.js")
 		);
 
-		Oops.folderToFile(uri).then(() => {
-			assert.ok(fs.lstatSync(uri.fsPath).isFile());
-		});
-
-		Oops.fileToFolder(uri).then(() => {
-			assert.ok(fs.lstatSync(uri.fsPath).isDirectory());
-		});
-	});
-
-	test("Should change file to folder", () => {
-		const uri = vscode.Uri.file(
-			path.join(__dirname + testFolderLocation + "oops_folder")
-		);
-
 		fs.rename(
-			testFolderLocation + "oops_folder/.gitkeep",
+			testFolderLocation + "oops_file.js/.gitkeep",
 			testFolderLocation + ".gitkeep",
 			() => {
-				Oops.fileToFolder(uri).then(() => {
-					assert.ok(fs.lstatSync(uri.fsPath).isDirectory());
-				});
-
 				Oops.folderToFile(uri).then(() => {
 					assert.ok(fs.lstatSync(uri.fsPath).isFile());
 				});
 
+				Oops.fileToFolder(uri).then(() => {
+					assert.ok(fs.lstatSync(uri.fsPath).isDirectory());
+				});
+
 				fs.rename(
 					testFolderLocation + ".gitkeep",
-					testFolderLocation + "oops_folder/.gitkeep",
+					testFolderLocation + "oops_file.js/.gitkeep",
 					() => { }
 				);
 			}
@@ -54,6 +40,23 @@ suite("Extension Test Suite", () => {
 		await Oops.folderToFile(uri);
 		assert.ok(fs.lstatSync(uri.fsPath).isDirectory());
 		assert.ok(fs.lstatSync(uri.fsPath + "/directory_with_children").isFile());
+	});
+
+	test("Should change file to folder", () => {
+		// Mistake folder
+		const uri = vscode.Uri.file(
+			path.join(__dirname + testFolderLocation + "oops_folder")
+		);
+
+		// Test converting to folder
+		Oops.fileToFolder(uri).then(() => {
+			assert.ok(fs.lstatSync(uri.fsPath).isDirectory());
+		});
+
+		// Revert
+		Oops.folderToFile(uri).then(() => {
+			assert.ok(fs.lstatSync(uri.fsPath).isFile());
+		});
 	});
 
 	test("Should not be able to delete not empty file", async () => {
